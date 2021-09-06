6da221004d100100316ec01d426f093a87e1cc4b
Ivar <ivar110kg@gmail.com>
2021-09-06 20:18:33 +0400

    Add form

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 
 def index(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()   # в случае со связ формой
+        return redirect('home')  # редирект
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
