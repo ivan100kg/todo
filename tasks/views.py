24f9cea5f3c9f8edbf555b832918b65d3e2e9bb0
Ivar <ivar110kg@gmail.com>
2021-09-06 20:45:24 +0400

    Add update form for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -9,7 +9,19 @@ def index(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()   # в случае со связ формой
-        return redirect('home')  # редирект
+            form.save()  # в случае со связ формой
+        return redirect('/')  # редирект
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(pk=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()  # в случае со связ формой
+        return redirect('/')  # редирект
+    context = {'form': form}
+    return render(request, 'tasks/update_task.html', context)
